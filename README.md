b1d575c6a95356390e6e993b2018becfb59a300d
Helias <stefanoborzi32@gmail.com>
2020-06-28 23:26:38 +0200

    import the project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # EPUB-to-PDF
-Telegram bot EPUB to PDF converter
+Telegram bot EPUB to PDF converter, this bot is available on Telegram with the following username: [@epub_to_pdf_bot](https://t.me/epub_to_pdf_bot)
+
+![EPUB to PDF](icon.png)
+
+### Usage
+
+Just contact the Telegram bot [@epub_to_pdf_bot](https://t.me/epub_to_pdf_bot) and send a file.epub, you will receive back a file.pdf.
+
+
+## Local installation (development)
+
+### Requirements
+
+- python3
+- python-telegram-bot
+- ebook-convert
+
+If you are using Linux as OS you could install the requirements using the package manager, for debian-based you could use the following command to install the main requirements:
+```
+sudo apt install python3 python3-pip ebook-convert
+```
+
+To install the python-telegram-bot framework you can use `pip` the python package manager:
+```
+pip3 install python-telegram-bot
+```
+
+### Configure
+
+Create a personal bot contacting [@Botfather](https://t.me/Botfather).
+Created the bot you will receive an API token, so copy the file *token.conf.dist* into *token.conf*, delete the content and put inside the API token.
+
+Well, now, you can run the script **main.py** and send to your bot the file.epub, you will get a file,pdf back.
+
+### Credits
+- [Helias](https://t.me/Helias)
---
+++ main.py
@@ -0,0 +1,45 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler, CallbackQueryHandler, Filters
+
+import logging
+import os
+import sys
+
+# get token from token.conf
+TOKEN = open("token.conf", "r").read().strip()
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def start(bot, update):
+  update.message.reply_text('Hi! Send me an epub I will convert it into PDF!')
+
+def conversion(bot, update):
+
+    if update.message.document:
+        file_name=update.message.document.file_name
+        file = bot.getFile(update.message.document.file_id)
+        file.download(file_name)
+        update.message.reply_text('Processing...' + file_name)
+        file_pdf = file_name.replace(".epub", ".pdf")
+        os.system("ebook-convert " + file_name + " " + file_pdf) # conversion to PDF
+        bot.send_document(chat_id=update.message.chat_id, document=open(file_pdf, 'rb'), caption="Here your PDF!")
+    else:
+        update.message.reply_text('Send me a file.epub I will convert it into PDF')
+        return
+
+def main():
+  updater = Updater(TOKEN)
+
+  dp = updater.dispatcher
+
+  dp.add_handler(MessageHandler(Filters.document, conversion))
+  dp.add_handler(CommandHandler('start', start))
+  dp.add_handler(CommandHandler('help', start))
+
+  updater.start_polling()
+  updater.idle()
+
+if __name__ == '__main__':
+    main()
