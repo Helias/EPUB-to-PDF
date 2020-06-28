1cf51e0b84bba13821c46d121d01000ce14c70cf
Helias <stefanoborzi32@gmail.com>
2020-06-28 23:50:32 +0200

    chore(file): remove file after conversion and upload

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def conversion(bot, update):
         file_pdf = file_name.replace(".epub", ".pdf")
         os.system("ebook-convert " + file_name + " " + file_pdf) # conversion to PDF
         bot.send_document(chat_id=update.message.chat_id, document=open(file_pdf, 'rb'), caption="Here your PDF!")
+        os.remove(file_name)
+        os.remove(file_pdf)
     else:
         update.message.reply_text('Send me a file.epub I will convert it into PDF')
         return
