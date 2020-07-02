1e246d404239193cdb61b491071f792c98fe7730
Helias <stefanoborzi32@gmail.com>
2020-07-02 20:37:37 +0200

    refactor(python-telegram): fix warning, upgrade API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler, CallbackQueryHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler, CallbackQueryHandler, Filters, CallbackContext
 
 import logging
 import os
@@ -12,19 +12,19 @@ TOKEN = open("token.conf", "r").read().strip()
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def start(bot, update):
+def start(update: Update, context: CallbackContext):
   update.message.reply_text('Hi! Send me an epub I will convert it into PDF!')
 
-def conversion(bot, update):
+def conversion(update: Update, context: CallbackContext):
 
     if update.message.document:
         file_name=update.message.document.file_name
-        file = bot.getFile(update.message.document.file_id)
+        file = context.bot.getFile(update.message.document.file_id)
         file.download(file_name)
         update.message.reply_text('Processing...' + file_name)
         file_pdf = file_name.replace(".epub", ".pdf")
         os.system("ebook-convert '" + file_name + "' '" + file_pdf + "'") # conversion to PDF
-        bot.send_document(chat_id=update.message.chat_id, document=open(file_pdf, 'rb'), caption="Here your PDF!")
+        context.bot.send_document(chat_id=update.message.chat_id, document=open(file_pdf, 'rb'), caption="Here your PDF!")
         os.remove(file_name)
         os.remove(file_pdf)
     else:
@@ -32,7 +32,7 @@ def conversion(bot, update):
         return
 
 def main():
-  updater = Updater(TOKEN)
+  updater = Updater(TOKEN, use_context=True)
 
   dp = updater.dispatcher
 
